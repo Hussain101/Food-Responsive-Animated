d5c4f153707aaef6f62469a916915c2e31f1fc1b
Hussain <hussainsidd88@gmail.com>
2021-09-09 11:17:14 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = ()=>{
+
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll= ()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-up').classList.add('act');
+    }else{
+        document.querySelector('#scroll-up').classList.remove('act');
+    }
+}
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+
+}
+
+function fadeOut() {
+    setInterval(loader, 3000);
+}
+window.onload = fadeOut();
+
+
